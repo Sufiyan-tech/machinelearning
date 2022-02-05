e23690bb3ceeb8bc3f78dd238fd711e0a3f44422
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-02-05 15:38:41 +0500

    First Python Series

---
+++ first.py
@@ -0,0 +1,17 @@
+print('Welcome Sufiyan To Machine Learning.....')
+print('Hello World')
+
+print(1+3)
+print(2*4)
+print(22/7)
+print(22//7)
+print(22%7)
+
+print('Python String Are Very Easy To Use')
+print('Hello' + ' World')
+
+greeting = 'Hello'
+name = 'Sufiyan'
+print(greeting + name)
+print(greeting + ' ' + name)
+print('We Can Use Double "quotes" in string')
