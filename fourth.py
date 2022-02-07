a38a92ef8ebf02c396f9f773601fb3f5ca96a9a1
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-02-07 08:32:24 +0500

    Control Flow and identation

---
+++ fourth.py
@@ -0,0 +1,8 @@
+for i in range(1,12):
+    print('Value If I : ' + str(i))
+print('For Loop Completed!')
+
+
+for var1 in range(1,12):
+    print('No.{} sqaured is {} and cubed is {}'.format(var1 , var1**2 , var1**3))
+print('For Loop Completed!')
