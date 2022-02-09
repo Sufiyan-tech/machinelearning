7cc4236a209d12da04167eed93cfd6894f1e52b2
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-02-09 08:56:55 +0500

    Input and escape

---
+++ sixth.py
@@ -0,0 +1,28 @@
+# greeting = 'Hello'
+# name = input('Enter Your Name : ')
+
+# var1 = int(input('Enter The Value : '))
+# print(var1)
+# print(type(var1))
+
+
+# var1 = input('Enter The String : ')
+# var1 = var1 * 2
+# print(var1)
+
+# splitString = 'Mera naam sufiyan aur \nmai machine learning engineer ban raha hoon'
+# print(splitString)
+
+# tabString = '1\t2\t3\t4\t5'
+# tabString1 = '1 2 3 4 5'
+# print(tabString)
+# print(tabString1)
+
+# print('What\'s Your Name')
+# print("What\"s Your Name")
+
+print('Mera naam sufiyan aur mai machine learning engineer ban raha hoon')
+print("Mera naam sufiyan aur \nmai machine learning engineer ban raha hoon")
+print("What's your name")
+print('''Mera naam sufiyan aur 
+mai machine learning engineer ban raha hoon''')
