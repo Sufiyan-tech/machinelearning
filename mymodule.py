f1c0c4a23f1b50759f9dbae8bb806bb45f9a126d
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-02-10 08:47:56 +0500

    Modules

---
+++ mymodule.py
@@ -0,0 +1,20 @@
+def myFunction():
+    print("Simple Function")
+
+
+def add(var1 , var2):
+    var3 = var1+var2
+    return var3
+
+def sub(var1 , var2):
+    var3 = var1-var2
+    return var3
+
+def mul(var1 , var2):
+    var3 = var1*var2
+    return var3
+
+def div(var1 , var2):
+    var3 = var1/var2
+    return var3
+
